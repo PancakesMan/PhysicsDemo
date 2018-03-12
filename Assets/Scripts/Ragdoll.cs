98fdfc6c53d832d80f34bc55e5f37b672ea47afe
ProProgrammerEric <eric_work@y7mail.com>
2018-03-13 09:08:06 +1030

    Initial Commit

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Ragdoll : MonoBehaviour {
+
+    private Animator animator = null;
+    public List<Rigidbody> rigidbodies = new List<Rigidbody>();
+
+    public bool RagdollOn {
+        get { return !animator.enabled; }
+        set
+        {
+            animator.enabled = !value;
+            foreach (Rigidbody r in rigidbodies)
+                r.isKinematic = !value;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rigidbodies.AddRange(GetComponentsInChildren<Rigidbody>());
+
+        foreach (Rigidbody r in rigidbodies)
+            r.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
